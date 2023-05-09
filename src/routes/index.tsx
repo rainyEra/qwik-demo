58d7d51e575ac2dd72836540cef7be98c95a394c
Netty <256truthx@gmail.com>
2023-05-09 21:54:33 +0000

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,40 +8,46 @@ export default component$(() => {
       { name: "Annet", active: false, key: crypto.randomUUID() },
       { name: "Evangelina", active: true, key: crypto.randomUUID() },
       { name: "Jerry", active: false, key: crypto.randomUUID() },
-      { name: "Walter", active: true, key: crypto.randomUUID() },
+      { name: "Noah", active: true, key: crypto.randomUUID() },
     ],
-    count: 0,
-    activate: $(function (target: number) {}),
+    activate: $(function (target: number, value: boolean) {
+      this.list[target].active = value;
+      console.log(this.list[target]);
+    }),
+    addMore: $(function (item) {
+      this.list.push(item);
+      console.log(this.list);
+    }),
   });
+
   return (
     <>
       <div class={styles.app}>
         {state.list.map((v, i) => (
-          <p class={v.active ? styles.active : null} key={v.key}>
+          <p class={v.active ? styles.active : ""} key={v.key}>
             <input
               type="checkbox"
               checked={v.active ? true : false}
-              onClick$={() => {
-                const value = state.list[i].active!;
-                state.list[i].active = true;
-                console.log("value", value);
-                console.log(state.list[i]);
+              onChange$={(e) => {
+                console.log(e.target.checked);
+                state.activate(i, e.target.checked);
               }}
             />
             {v.name}
           </p>
         ))}
+        <button
+          onClick$={() => {
+            state.addMore({
+              name: "ASD",
+              active: true,
+              key: crypto.randomUUID(),
+            });
+          }}
+        >
+          Add more!
+        </button>
       </div>
     </>
   );
 });
-
-export const head: DocumentHead = {
-  title: "Welcome to Qwik",
-  meta: [
-    {
-      name: "description",
-      content: "Qwik site description",
-    },
-  ],
-};
